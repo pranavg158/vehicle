9bd1bf92e6ef3fc438b103c139939f86a9b4a15c
pranavg158 <46554015+pranavg158@users.noreply.github.com>
2022-05-11 10:39:07 +0530

    Update streamlib_new.py

--- streamlib_new.py
+++ streamlib_new.py
@@ -178,8 +178,10 @@ def trackMultipleObjects():
                       if speed[i] != None and y1 >= 180:
                           cv2.putText(resultImage, str(int(speed[i])) + "km/h", (int(x1 + w1/2), int(y1-5)), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 100) ,2)
 
-          cv2.imshow('result', resultImage)
-        
+          #cv2.imshow('result', resultImage)
+          image1=Image.open(resultImage)
+          st.image(image1, width=1280)
+            
 
           out.write(resultImage)
 
